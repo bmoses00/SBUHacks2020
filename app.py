ef93ff46ee8848e4e6fb7a1c73f5740535a63ac1
Brian Moses <bmoses00@stuy.edu>
2020-09-25 20:31:00 -0400

    Added geojson map

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import configparser
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    config = configparser.ConfigParser()
+    config.read('properties.ini')
+    return render_template("index.html", key = config["DEFAULT"]["Key"])
 
 if __name__ == "__main__":
         app.debug = True
