cbd352d83ea514d10b8138046ad5587af40624a1
Greg Zborovsky <greg.zbo3@gmail.com>
2020-09-25 22:59:07 -0400

    fixed

--- static/js/script.js
+++ static/js/script.js
@@ -15,13 +15,12 @@ function initMap() {
             },
             strictBounds: false,
         },
-      strictBounds: false,
-    },
-    center: new google.maps.LatLng(coords[0], coords[1]),
-    mapTypeId: 'terrain',
-    mapTypeControlOptions: { mapTypeIds: [] },
-    streetViewControl:false
-  });
+        strictBounds: false,
+        center: new google.maps.LatLng(coords[0], coords[1]),
+        mapTypeId: 'terrain',
+        mapTypeControlOptions: { mapTypeIds: [] },
+        streetViewControl: false
+    });
 
 
 
@@ -38,34 +37,34 @@ function initMap() {
     // );
     document.getElementsByTagName('head')[0].appendChild(script);
 
-  var infowindow = null;
-  var marker = null;
+    var infowindow = null;
+    var marker = null;
 
-  map.data.addListener('mouseover', function(event) {
+    map.data.addListener('mouseover', function (event) {
 
-    let precinct = event.feature.j.precinct;
-    if (infowindow != null) infowindow.close();
-    infowindow = new google.maps.InfoWindow({
-      content: "Precinct:" + precinct,
+        let precinct = event.feature.j.precinct;
+        if (infowindow != null) infowindow.close();
+        infowindow = new google.maps.InfoWindow({
+            content: "Precinct:" + precinct,
+        });
+
+        if (marker != null) marker.setMap(null);
+        marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: 'marker'
+        });
+        marker.setVisible(false);
+
+        infowindow.open(map, marker);
     });
-
-    if (marker != null) marker.setMap(null);
-    marker = new google.maps.Marker({
-      position: event.latLng,
-      map: map,
-      title: 'marker'
+    map.data.addListener('mouseout', function (event) {
+        if (infowindow != null) infowindow.close();
+        if (marker != null) marker.setMap(null);
     });
-    marker.setVisible(false);
-
-    infowindow.open(map, marker);
-  });
-  map.data.addListener('mouseout', function(event) {
-      if (infowindow != null) infowindow.close();
-      if (marker != null) marker.setMap(null);
-  });
-  map.data.addListener('click', function(event) {
-      console.log('hi');
-  });
+    map.data.addListener('click', function (event) {
+        console.log('hi');
+    });
 
 }
 
